e9e1547956e58ee228fd49cb4bae35b8b56284c1
kumkum chaudhary <kumkum.chaudhary@atsweb.onmicrosoft.com>
2026-09-11 16:44:54 +0530

    Add files via upload

---
+++ python/typecasting.py
@@ -0,0 +1,12 @@
+num1 = input("Enter number 1: ")
+num2 = input("Enter number 2: ")
+
+ans = num1 + num2
+print(ans)
+
+"""
+typecasting : 
+   convert one datatype value into another data type.
+
+   e.g., int(value)
+"""
---
+++ python/variable.py
@@ -0,0 +1,10 @@
+"""
+ variable : variable is a name which can store a value.
+
+ in python there is no need to declare any variable.
+"""
+name ="Python"
+marks = 90
+print(name)
+print(marks)
+
